7419a920320031b6eb3024240834e024f715b7d1
Z-LianYan <1371935318@qq.com>
2026-05-10 00:59:36 +0800

    1

--- app/models/admin.py
+++ app/models/admin.py
@@ -1,8 +1,16 @@
-from pydantic import BaseModel,Field,ConfigDict
+from pydantic import BaseModel,Field,ConfigDict, field_validator
 from typing import Optional,List
-
 class AdminLoginParams(BaseModel):
-    phone: str = Field(..., min_length=11, max_length=11,title="手机号", description="手机号")
+    phone: str = Field(..., description="手机号")
     password: str = Field(..., min_length=6, max_length=32, description="密码")
     captchaCode: str  = Field(..., min_length=4, max_length=4, description="验证码")
-    captchaKey: str = Field(..., max_length=200, description="captchaKey")
+    captchaKey: str = Field(..., max_length=200, description="验证码key")
+
+    @field_validator("phone")
+    @classmethod
+    def validate_phone(cls, v):
+        v= v.replace(" ", "")
+        if len(v) != 11:
+            raise ValueError("手机号必须11位")
+
+        return v
