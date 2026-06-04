0fc8879f7e793d8dedb34387fa6a8a13a797b91c
Z-LianYan <1371935318@qq.com>
2026-06-05 02:00:33 +0800

    1

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends
+import datetime
+
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.utils.httpRes import ResStructure
 from jose import jwt
@@ -13,10 +15,11 @@ from app.utils.httpRes import success,fail
 from app.redis.redis import redis_manager
 from app.models.admin import AdminLoginParams
 
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from app.db.deps import get_db
 from app.db.models.user import User
-
+from app.core.guards.authLogin import login_auth_guard
 router = APIRouter()
 
 # @router.get("/getCaptcha",description="获取验证码返回图片",summary="获取验证码")
@@ -95,21 +98,122 @@ def create_item(
     try:
         res = jwt.decode(token, settings.JWT_SECRET, algorithms=["HS256"])
 
-        result = db.query(User).all()
+        result = (db.query(User.username,func.count(User.username).label("count"))
+                  .filter(
+                User.password == "1",
+                        User.id>=1,
+                        User.id.in_([8,9,10]),
+                        User.username.like("%Tom%")
+                    )
+                  .group_by(User.username)
+                  .having(User.username=="Tom")
+                  .order_by(User.username.desc())
+                  .offset(0)
+                  .limit(10)
+                  .all())
         data = [
             {
-                "id": item.id,
                 "username": item.username,
-                "password": item.password,
-                "created": item.created,
+                "count": item.count
             }
             for item in result
         ]
 
         print("sql===>>",data)
+        # 新增
+        #1
+        user = User(
+            username="Tom",
+            password="1",
+            created=datetime.datetime.now(),
+        )
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        print("create===user", user.id) # 获取新增后的id
+        #2 新增多条数据
+        users = [
+            User(
+                username="老六1",
+                password="1",
+                created=datetime.datetime.now(),
+            ),
+            User(
+                username="老六2",
+                password="1",
+                created=datetime.datetime.now(),
+            ),
+            User(
+                username="老六3",
+                password="1",
+                created=datetime.datetime.now(),
+            )
+        ]
+        db.add_all(users)
+        db.commit()
+
+        # 修改
+        #1
+        user = (
+            db.query(User)
+            .filter(User.id == 12)
+            .first()
+        )
+        if user:
+            user.username = "Tom12"
+            db.commit()
+        #2
+        db.query(User).filter(
+            User.id == 12
+        ).update({
+            User.username: "Tom1212"
+        })
+
+        db.commit()
+
+
+        # 删除
+        #1
+        user = (
+            db.query(User)
+            .filter(User.id == 14)
+            .first()
+        )
+
+        if user:
+            db.delete(user)
+            db.commit()
+
+        #2
+        db.query(User).filter(
+            User.id == 15
+        ).delete()
+
+        db.commit()
+
+
+        # 事务回滚
+        try:
+
+            user = User(
+                username="王五33",
+                password="1",
+                created=datetime.datetime.now(),
+            )
+
+            db.add(user)
+            db.commit()
+        except Exception:
+            db.rollback()
+            raise
+
+
         return success({
             "code": 200,
-            "data": data,
+            "data": {
+                "data": data,
+                "token": token
+            },
             "msg": "操作成功"
         })
     except Exception as e:
